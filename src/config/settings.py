49e172d13111d283b68fd97cac454f7721a2e5bb
Vemestael <volodimir.grigoryev@gmail.com>
2022-08-11 17:08:43 +0300

    replace nginx and certbot with an out-of-the-box image

--- src/config/settings.py
+++ src/config/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Get environment variables from .env file
 env = environ.Env(
     DEBUG=(bool, False),
-    ALLOWED_HOSTS=(list, ['.localhost', '127.0.0.1', '[::1]'])
+    ALLOWED_HOSTS=(list, ['.localhost', '127.0.0.1', '[::1]']),
+    CSRF_TRUSTED_ORIGINS=(list, ['https://*.127.0.0.1'])
 )
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
@@ -35,6 +36,8 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 # Application definition
 
